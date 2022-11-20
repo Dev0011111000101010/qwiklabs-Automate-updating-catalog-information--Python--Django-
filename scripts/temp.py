1faf2694b3b90b5446c7782d849fb493b855c0bc
Dev0011111000101010 <62564629+Dev0011111000101010@users.noreply.github.com>
2022-11-20 08:11:53 -0500

    Convenience: commands have been added to the temp.py file that need to be run manually in the console

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,17 +1,9 @@
 chmod 600 ~/Downloads/qwiklabs-L36124082.pem
 ssh -i ~/Downloads/qwiklabs-L36124082.pem student-00-350ac9c49fda@35.225.10.158
 
-cat ~/supplier-data/descriptions/007.txt
+sudo apt-get update
+sudo apt install python-django-common
+sudo systemctl start google-startup-scripts.service
+
 nano  ~/supplier-data/descriptions/010.txt
-
-
-1
-
-
-
-
-
-
-
-
-10
+nano  ~/supplier-data/descriptions/001.txt
