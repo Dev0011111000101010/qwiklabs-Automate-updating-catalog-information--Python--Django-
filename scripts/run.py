54fcd5962dbfbfea2550a7de6ca85d6877bee98e
Dev0011111000101010 <62564629+Dev0011111000101010@users.noreply.github.com>
2022-11-20 07:07:46 -0500

    Test: locally TXT files are successfully processed in DICTIONARY for Django
    run.py displays data from .txt files and visually, everything matches the requirements.
    
    Can be tested on VM.

--- scripts/run.py
+++ scripts/run.py
@@ -5,6 +5,11 @@
 # sudo apt install python-django-common
 # sudo systemctl start google-startup-scripts.service
 
+"""First, before running
+    run.py
+    , you need to run the file
+    run_first.py """
+
 import os
 import requests
 import json
@@ -36,66 +41,6 @@ print('----------------------')
 print('# QA: Result: If everything is correct, it should return a list of TXT feedback files.')
 print(os.listdir(fruits_list_txt_files_folder_path))
 print('----------------------')
-
-def change_TXT_files_2_automate_uploading(fruits_list_txt_files_folder_path):
-    """1. Reading the contents of TXT files in a folder 'fruits_list_txt_files_folder_path'
-       2. In weight, remove "lbs"
-       3. Add the name of the image to the end of TXT"""
-
-    # Get the list of all files and directories
-    # in the 'fruits_list_txt_files_folder_path' directory
-    print('======================')
-    print('----------------------')
-    print('# QA: Result: If everything is correct, it should return a list of TXT files.')
-    print('# Difference from above: This test is already running from the "change_TXT_files_2_automate_uploading" function')
-    print(os.listdir(fruits_list_txt_files_folder_path))
-    print('----------------------')
-
-    # Creating a variable for a list of TXT files
-    txt_files_list = os.listdir(fruits_list_txt_files_folder_path)
-    # QA: Iterating over each individual feedback file
-    for file in txt_files_list:
-        print(file + '    # QA : Iterating over each individual feedback file')
-
-    print('----------------------')
-    print('----------------------')
-    print('# QA : Iterating over each individual feedback file content')
-    print('# QA : The second line shows the converted dictionary')
-
-    for file in txt_files_list:
-        # https://ru.stackoverflow.com/questions/654183/%D0%9A%D0%B0%D0%BA-%D0%B7%D0%B0%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D1%81%D1%82%D1%80%D0%BE%D1%87%D0%BA%D1%83-%D0%B2-txt-%D1%84%D0%B0%D0%B9%D0%BB%D0%B5-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-python-3
-        # Remove "lbs"
-        with open(fruits_list_txt_files_folder_path + file, 'r') as f:
-            old_data = f.read()
-
-
-        # What are we looking for and what are we changing at?
-        new_data = old_data.replace(' lbs\n', '\n')
-        with open(fruits_list_txt_files_folder_path + file, 'w') as f:
-            f.write(new_data)
-            print(file + "# Changed ' lbs' => ''")
-
-    # Trying to write code that is reusable and won't add extra image names
-    # https://stackoverflow.com/questions/44205923/checking-if-word-exists-in-a-text-file-python
-    # search_word = input(image_name)
-    # if search_word in open(fruits_list_txt_files_folder_path + file, 'a').read():
-    #     print('# The script for changing the TXT file has been launched repeatedly')
-    #     print('# "lbs" has already been removed from the TXT file')
-    #     print('# Image title already added to TXT files')
-    #     print('# No additional action required')
-    #     pass
-    # else:
-    #     with open(fruits_list_txt_files_folder_path + file, 'a') as f:
-    #         f.write('\n' + image_name)
-
-    # https://www.geeksforgeeks.org/python-append-to-a-file/
-    # Add to the end the name of the image file that matches the name of the TXT file
-    for file in txt_files_list:
-        with open(fruits_list_txt_files_folder_path + file, 'a') as f:
-            image_name = file[:4] + "jpeg"
-            f.write('\n' + image_name)
-            print(file + "# +name of the image")
-
 
 def txt_2_dictionary_jungo_structure(fruits_list_txt_files_folder_path):
     """1. Reading the contents of TXT files in a folder 'fruits_list_txt_files_folder_path'
@@ -132,17 +77,17 @@ def txt_2_dictionary_jungo_structure(fruits_list_txt_files_folder_path):
 
 
 
-            # # Hint: list to dictionary
-            # # https://careerkarma.com/blog/python-convert-list-to-dictionary/
-            # # fruits_list_dictionary = dict(zip(list_dictionary_titles, list_content_of_feedback_files))
-            # fruits_list_dictionary = dict(zip(list_dictionary_titles, list_content_of_feedback_files))
-            # print(fruits_list_dictionary)
+            # Hint: list to dictionary
+            # https://careerkarma.com/blog/python-convert-list-to-dictionary/
+            fruits_list_dictionary = dict(zip(list_dictionary_titles, list_content_of_feedback_files))
+            print(fruits_list_dictionary)
+            """For local use, comment out the line below, for online use, uncomment"""
             # post_each_1_fruit_in_json_format_2_server = requests.post(combat_server_url, json=fruits_list_dictionary)
-            # print('======================')
-            # print('----------------------')
-            # print(' # QA: print(json.dumps(fruits_list_dictionary))')
-            # print(json.dumps(fruits_list_dictionary))
-            # print('----------------------')
+            print('======================')
+            print('----------------------')
+            print(' # QA: print(json.dumps(fruits_list_dictionary))')
+            print(json.dumps(fruits_list_dictionary))
+            print('----------------------')
             # # Change single quotes in Python dictionary to Jason double quotes
             # # It turned out to be mandatory for the manual addition of feedback and interfere with the automatic one
             # # dictionary_double_quotes_formate_transform = json.dumps(fruits_list_dictionary)
@@ -153,24 +98,21 @@ def txt_2_dictionary_jungo_structure(fruits_list_txt_files_folder_path):
             # print('----------------------')
             # print('')
 
-    print('======================')
-    print('----------------------')
-    print(' # QA: response.status_code')
-    print(post_each_1_fruit_in_json_format_2_server.status_code)
-    print(' # QA: response.ok')
-    print(post_each_1_fruit_in_json_format_2_server.ok)
-    print('----------------------')
-    print(' # QA: response.request.url')
-    print(post_each_1_fruit_in_json_format_2_server.request.url)
-    print('----------------------')
-    print(' # QA: response.request.body')
-    print(post_each_1_fruit_in_json_format_2_server.request.body)
-    print('----------------------')
-    print('')
-
-# Modifying TXT files to automate downloads
-# In weight, remove "lbs", add the name of the image to the end
-change_txt_2_automate_uploading = change_TXT_files_2_automate_uploading(fruits_list_txt_files_folder_path)
+    """For local use, comment out the lines below, for online use, uncomment"""
+    # print('======================')
+    # print('----------------------')
+    # print(' # QA: response.status_code')
+    # print(post_each_1_fruit_in_json_format_2_server.status_code)
+    # print(' # QA: response.ok')
+    # print(post_each_1_fruit_in_json_format_2_server.ok)
+    # print('----------------------')
+    # print(' # QA: response.request.url')
+    # print(post_each_1_fruit_in_json_format_2_server.request.url)
+    # print('----------------------')
+    # print(' # QA: response.request.body')
+    # print(post_each_1_fruit_in_json_format_2_server.request.body)
+    # print('----------------------')
+    # print('')
 
 # Launching the "txt_2_dictionary_jungo_structure" function into which the "fruits_list_txt_files_folder_path" path is passed
-# convert_txt_fruits_lis_2_dictionary_jungo_structure = txt_2_dictionary_jungo_structure(fruits_list_txt_files_folder_path)
+convert_txt_fruits_lis_2_dictionary_jungo_structure = txt_2_dictionary_jungo_structure(fruits_list_txt_files_folder_path)
